3da4718694b5969540d607840e7134675d4879e8
PREM SAHITH <premsahith123@gmail.com>
2026-09-01 16:02:10 +0530

    Added contextLanguage to greeting.jsx

--- Attendance Tracker/src/greeting.jsx
+++ Attendance Tracker/src/greeting.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { LanguageContext } from "./LanguageContext.jsx";
+
 function Greeting(props) {
+  const { language, setLanguage } = useContext(LanguageContext);
   return (
     <div>
       <h1>Hello {props.name}!</h1>
+      <h1>Current Language: {language}</h1>
+      <button onClick={() => setLanguage("en")}>English</button>
+      <button onClick={() => setLanguage("hi")}>Hindi</button>
+      <button onClick={() => setLanguage("ta")}>Tamil</button>
+      <button onClick={() => setLanguage("es")}>Spanish</button>
     </div>
   );
 }
