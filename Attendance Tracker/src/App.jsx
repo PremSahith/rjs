e479bef5d06ec34cb8aee2e434ab176cb00456ed
PREM SAHITH <premsahith123@gmail.com>
2026-09-22 15:25:23 +0530

    Added Local storage

--- Attendance Tracker/src/App.jsx
+++ Attendance Tracker/src/App.jsx
@@ -5,8 +5,11 @@ function App() {
   const [present, setPresent] = useState(0);
   const [absent, setAbsent] = useState(50);
 
-  const [name, setName] = useState("");
-  const [submittedName, setSubmittedName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [submittedUsername, setSubmittedUsername] = useState("");
+
   const { language, setLanguage } = useContext(LanguageContext);
   return (
     <div>
@@ -32,19 +35,44 @@ function App() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          setSubmittedName(name);
+          //this e.preventDefault() is used to prevent the default behavior of the form submission, which is to reload the page. Instead, we want to handle the form submission in our own way, which is to update the submittedName state with the current value of the name state.
+
+          localStorage.setItem("username", username);
+          localStorage.setItem("password", password);
+
+          setSubmittedUsername(username);
         }}
       >
         <input
           type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-
-        <button type="submit">Submit</button>
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button
+          type="submit"
+          disabled={username.length < 5 || password.length < 5}
+        >
+          Submit
+        </button>
       </form>
-      {submittedName && <p>Submitted Name: {submittedName}</p>}
+      <p>Submitted Username: {submittedUsername}</p>
+      <button
+        onClick={() => {
+          localStorage.removeItem("username");
+          localStorage.removeItem("password");
+        }}
+      >
+        Clear Data
+      </button>
     </div>
   );
 }
